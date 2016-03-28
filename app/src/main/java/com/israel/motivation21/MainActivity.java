59ea53855d05c59a5fab317d42c97993e5baf8b8
yzraeu <yzraeu@gmail.com>
2016-03-28 19:08:09 -0300

    initial commit

--- app/src/main/java/com/israel/motivation21/MainActivity.java
+++ app/src/main/java/com/israel/motivation21/MainActivity.java
@@ -1,13 +1,41 @@
 package com.israel.motivation21;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int[] images = {R.drawable.churchill, R.drawable.jefferson, R.drawable.darwin, R.drawable.fitzgerald, R.drawable.cameron};
+    private int[] titles = {R.string.title_1, R.string.title_2, R.string.title_3, R.string.title_4, R.string.title_5};
+    private int[] tips = {R.string.tip_1, R.string.tip_2, R.string.tip_3, R.string.tip_4, R.string.tip_5};
+    private int[] phrases = { R.string.phrase_churchill, R.string.phrase_jefferson, R.string.phrase_darwin, R.string.phrase_fitzgerald, R.string.phrase_cameron};
+    private int[] authors = {R.string.name_churchill, R.string.name_jefferson, R.string.name_darwin, R.string.name_fitzgerald, R.string.name_cameron};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager vp = (ViewPager) findViewById(R.id.viewPager);
+        vp.setAdapter(new WelcomeAdapter(this, images, titles, tips, phrases, authors));
+
+        vp.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
+
---
+++ app/src/main/java/com/israel/motivation21/WelcomeAdapter.java
@@ -0,0 +1,89 @@
+package com.israel.motivation21;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v4.view.PagerAdapter;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Israel on 23/03/2016.
+ */
+public class WelcomeAdapter extends PagerAdapter {
+
+    private Context context;
+    private int[] images;
+    private int[] titles;
+    private int[] tips;
+    private int[] phrases;
+    private int[] authors;
+
+    public WelcomeAdapter(Context context, int[] imgs, int[] titles, int[] tips, int[] phrases, int[] authors) {
+        this.context = context;
+        this.images = imgs;
+        this.titles = titles;
+        this.tips = tips;
+        this.phrases = phrases;
+        this.authors = authors;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = li.inflate(R.layout.tip_page_layout, null);
+
+        CharSequence title = getResourceText(titles[position]);
+        CharSequence tip = getResourceText(tips[position]);
+        CharSequence phrase = getResourceText(phrases[position]);
+        CharSequence author = getResourceText(authors[position]);
+
+        ImageView imageView = (ImageView) v.findViewById(R.id.back_image);
+        imageView.setImageResource(images[position]);
+
+        TextView titleTextView = (TextView) v.findViewById(R.id.title_view);
+        titleTextView.setText(title);
+
+        TextView tipTextView = (TextView) v.findViewById(R.id.tip_view);
+        tipTextView.setText(tip);
+
+        TextView phraseTextView = (TextView) v.findViewById(R.id.phrase_view);
+        phraseTextView.setText("\"" + phrase + "\"");
+
+        TextView authorTextView = (TextView) v.findViewById(R.id.author_view);
+        authorTextView.setText(" – " + author);
+
+        container.addView(v);
+
+        return v;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+//        Log.i("Script", "Destroy car: " + (position + 1));
+
+        container.removeView((View) object);
+
+//        super.destroyItem(container, position, object);
+    }
+
+    private CharSequence getResourceText(int resId) {
+        return context.getResources().getText(resId);
+    }
+}
